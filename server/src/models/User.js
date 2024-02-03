7bbed743e8f2532192c41d2ec90c6e3101a92a09
Shreedhar Acharya <spacharya2537@gmail.com>
2024-02-03 11:08:57 -0500

    [FW-18]UserSchema model for signup

---
+++ server/src/models/User.js
@@ -0,0 +1,36 @@
+/*
+    ---------------------------------------------------
+    Author      : Shree Dhar Acharya
+    StudentId   : 8899288
+    Date        : 2nd Feb 2024
+    Application : FoodWise
+    ----------------------------------------------------
+*/
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  
+  userType: {
+    type: String,
+    enum: ['Admin','Staff', 'User', 'Doner', 'Receiver'],
+    required: true,
+  },
+  userStatus: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
