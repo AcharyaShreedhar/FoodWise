d93bb1f74dac282f60f1fc6c6f9b4013a383e82b
Shreedhar Acharya <spacharya2537@gmail.com>
2024-02-03 13:34:11 -0500

    [FW-18]Creating salt for password and saving as  salt using mdfhashing and salt

--- server/src/resolvers/userResolver.js
+++ server/src/resolvers/userResolver.js
@@ -8,26 +8,51 @@
 */
 
 const User = require('../models/User');
+const crypto = require('crypto');
 
-const userResolvers = {
+// Function to generate a random salt
+const generateSalt = (length = 16) => {
+    return crypto.randomBytes(Math.ceil(length / 2))
+      .toString('hex')
+      .slice(0, length);
+  };
   
-  Mutation: {
-    createUser: async (_, args) => {
-      const { email, password, userType, userStatus } = args;
+  // Function to hash the password with the salt
+  const hashPassword = (password, salt) => {
+    const hash = crypto.createHmac('sha256', salt);
+    hash.update(password);
+    return hash.digest('hex');
+  };
+  
 
-      try {
-        const newUser = new User({
-            email, password, userType, userStatus
-        });
-        const savedUser = await newUser.save();
-        return savedUser;
-      } catch (error) {
-        console.error('Error creating user account:', error);
-        throw new Error(`Failed to create user account: ${error.message}`);
-      }
+  const userResolvers = {
+    Mutation: {
+      createUser: async (_, args) => {
+        const { email, password, userType, userStatus } = args;
+  
+        try {
+             // Generate a random salt for password hashing
+          const salt = generateSalt();
+          //Hash the password with the generated salt
+          const hashedPassword = hashPassword(password, salt);
+        
+           // Create a new User instance with the hashed password and other details
+          const newUser = new User({
+            email,
+            password: hashedPassword,
+            salt,
+            userType,
+            userStatus,
+          });
+         // Save the new user to the database
+          const savedUser = await newUser.save();
+          return savedUser;
+        } catch (error) {
+          console.error('Error creating user account:', error);
+          throw new Error(`Failed to create user account: ${error.message}`);
+        }
+      },
     },
-    
-},
-};
+  };
 
 module.exports = userResolvers;
