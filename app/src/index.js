e375b4ca1902058695888585b704231c45c0e3f6
Shreedhar Acharya <spacharya2537@gmail.com>
2024-02-04 20:51:19 -0500

    [FW-26]using the Apollo Client to connect to your GraphQL server in the React application

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+    ---------------------------------------------------
+    Author      : Shree Dhar Acharya
+    StudentId   : 8899288
+    Date        : 4th Feb 2024
+    Application : FoodWise
+    ----------------------------------------------------
+*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4021/graphql",
+  cache: new InMemoryCache(),
+  onError: (error) => {
+    console.error("Apollo Client Error:", error);
+  },
+});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
