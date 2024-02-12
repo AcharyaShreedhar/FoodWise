8ba492c08388a5392e6f949a4f5468b30ed22be9
Shreedhar Acharya <spacharya2537@gmail.com>
2024-02-11 21:45:38 -0500

    [FW-32]Schema definition for a product in a MongoDB database

---
+++ server/src/models/Product.js
@@ -0,0 +1,45 @@
+/*
+    ---------------------------------------------------
+    Author      : Shree Dhar Acharya
+    StudentId   : 8899288
+    Date        : 2nd Feb 2024
+    Application : FoodWise
+    ----------------------------------------------------
+*/
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+  },
+  
+  userType: {
+    type: String,
+    enum: ['Admin','Staff', 'User', 'Donor', 'Receiver'],
+    required: true,
+  },
+  userStatus: {
+    type: Boolean,
+    default: false,
+  },
+  resetToken: {
+    type: String,
+  },
+  resetTokenExpiry: {
+    type: Date,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
