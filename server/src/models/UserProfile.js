969a74f2f0bd5effd3d5021ffe12ac6f9a068605
Shreedhar Acharya <spacharya2537@gmail.com>
2024-03-13 20:29:09 -0400

    [FW-61]User Profile model

---
+++ server/src/models/UserProfile.js
@@ -0,0 +1,35 @@
+/*
+    ---------------------------------------------------
+    Author      : Shree Dhar Acharya
+    StudentId   : 8899288
+    Date        : 13th March 2024
+    Application : FoodWise
+    ----------------------------------------------------
+*/
+
+
+const mongoose = require('mongoose');
+
+const userProfileSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  phoneNumber:{
+    type:String,
+  },
+  address:{
+    type:String,
+  }
+});
+
+const UserProfile = mongoose.model('UserProfile', userProfileSchema);
+
+module.exports = UserProfile;
