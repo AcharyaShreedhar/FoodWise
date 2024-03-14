a079efc094a2853ea8eb47864d5a61d7547f88c9
Shreedhar Acharya <spacharya2537@gmail.com>
2024-03-13 20:51:39 -0400

    [FW-61]User Profile schema

---
+++ server/src/schemas/userProfileSchema.js
@@ -0,0 +1,57 @@
+/*
+    ---------------------------------------------------
+    Author      : Shree Dhar Acharya
+    StudentId   : 8899288
+    Date        : 13th March 2024
+    Application : FoodWise
+    ----------------------------------------------------
+*/
+
+const { gql } = require("apollo-server-express");
+
+const userProfileSchema = gql`
+  type UserProfile {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    dateOfBirth: String!
+    phoneNumber: String!
+    address: String!
+  }
+
+  type Query {
+    userProfiles(input: UserProfileSearchInput): [UserProfile]!
+    userProfile(id: ID!): UserProfile
+  }
+
+  type Mutation {
+    createUserProfile(
+        firstName: String!
+        lastName: String!
+        dateOfBirth: String!
+        phoneNumber: String!
+        address: String!
+    ): UserProfile
+  }
+
+  type Mutation {
+    updateUserProfile(input: UserProfileInput!): UserProfile
+    deleteUserProfile(userProfileId: ID!): Boolean
+  }
+
+  input UserProfileInput {
+    userProfileId: ID!
+    firstName: String
+    lastName: String
+    dateOfBirth: String
+    phoneNumber: String
+    address: String
+  }
+
+  input UserProfileSearchInput {
+    firstName: String
+    
+  }
+`;
+
+module.exports = userProfileSchema;
